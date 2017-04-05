43e7bad089157ccb97bc6ea7aebf8b42da0f8d2c
X <allsuper@foxmail.com>
2017-04-06 00:01:17 +0800

    整加方法

--- connect.js
+++ connect.js
@@ -1,7 +1,12 @@
+function l(n) {
+    console.log(n);
+}
+
 //引包
 var mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://localhost/haha");
+
 //创建一个模型
 var Cat = mongoose.model("Cat", {name: String});
 //实例一个猫
@@ -16,13 +21,11 @@ kitty.save(function (err) {
 });
 
 var tom = new Cat({name: "汤姆"});
-tom.save(function(err){
+tom.save(function (err) {
     if (err) {
         l(err);
         return;
     }
     l("汤姆保存成功");
 });
-function l(n) {
-    console.log(n);
-}
+
